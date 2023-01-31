74eb19e9ad9984de1d7ae2aa77bd39791768635c
HemanthPaila <pailahemanth2002@gmail.com>
2023-01-31 17:51:52 +0530

    added states and dynamic styling to component elements

--- src/Form.js
+++ src/Form.js
@@ -4,13 +4,14 @@ const Form = () => {
   const [valid, isValid] = useState(true);
   const [valid1, isValid1] = useState(true);
   const [valid2, isValid2] = useState(true);
-  //   const [valid3, isValid3] = useState(true);
-  //   const [valid4, isValid4] = useState(true);
-  //   const [valid5, isValid5] = useState(true);
+  const [valid3, isValid3] = useState(true);
+  const [valid4, isValid4] = useState(true);
   const clickHandler = (event) => {
     isValid(true);
     isValid1(true);
     isValid2(true);
+    isValid3(true);
+    isValid4(true);
     const name = event.target.value;
     for (var i = 0; i < name.length; i++) {
       if (name.charCodeAt(i) >= 97 && name.charCodeAt(i) <= 122) {
@@ -21,6 +22,21 @@ const Form = () => {
       }
       if (name.length >= 8) {
         isValid2(false);
+      }
+      if (
+        name.charAt(i) === "!" ||
+        name.charAt(i) === "@" ||
+        name.charAt(i) === "#" ||
+        name.charAt(i) === "$" ||
+        name.charAt(i) === "%" ||
+        name.charAt(i) === "^" ||
+        name.charAt(i) === "&" ||
+        name.charAt(i) === "*"
+      ) {
+        isValid3(false);
+      }
+      if (Number(name[i]) > 0) {
+        isValid4(false);
       }
     }
   };
@@ -36,9 +52,15 @@ const Form = () => {
       <li style={{ visibility: !valid ? "hidden" : "visible" }}>
         password should contain atleast one lowercase letter
       </li>
-      <li>password should contain atleast on number</li>
-      <li>password should contain atleast one special character</li>
-      <li>special characters like: !, @, #, $, %, ^, &, *</li>
+      <li style={{ visibility: !valid4 ? "hidden" : "visible" }}>
+        password should contain atleast on number
+      </li>
+      <li style={{ visibility: !valid3 ? "hidden" : "visible" }}>
+        password should contain atleast one special character
+      </li>
+      <li style={{ visibility: !valid3 ? "hidden" : "visible" }}>
+        special characters like: !, @, #, $, %, ^, &, *
+      </li>
     </div>
   );
 };
